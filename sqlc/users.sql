208a8e61dba7630e1980b6f78a14b8390f4d30e3
Nikita <71850431+badfan@users.noreply.github.com>
2022-05-24 10:47:00 +0300

    Feature models and migrations (#4)
    * struct init
    
    * struct init
    
    * struct init
    
    * struct init
    
    * docker init
    
    * docker and pre-commit-config init
    
    * dockerfile+docker-compose+entrypoint init
    
    * dockerfile+docker-compose+entrypoint init
    
    * dockerfile+docker-compose+entrypoint init
    
    * init models with sqlc and init migrations with goose
    
    * init models with sqlc and init migrations with goose
    
    * init models with sqlc and init migrations with goose

---
+++ sqlc/users.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id SERIAL PRIMARY KEY,
+    user_uuid uuid DEFAULT uuid_generate_v4 (),
+    name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(25) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_rating REAL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- name: CreateUser :one
+INSERT INTO users (name, phone_number, email, password)
+VALUES ($1, $2, $3, $4) RETURNING *;
+
+-- name: GetUsers :many
+SELECT * FROM users;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id = $1;
+
+-- name: GetUserPhoneByID :one
+SELECT phone_number FROM users
+WHERE id = $1;
+
+-- name: GetUserEmailByID :one
+SELECT email FROM users
+WHERE id = $1;
+
+-- name: GetUserRatingByID :one
+SELECT user_rating FROM users
+WHERE id = $1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET name = $1, phone_number = $2, email = $3, password= $4
+WHERE id=$5 RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
